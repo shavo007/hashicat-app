8c4d0c46a625f520234374aeb432782e266f1e86
shavo007 <shanelee007@gmail.com>
2024-11-12 03:36:12 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -200,6 +200,6 @@ resource "aws_key_pair" "hashicat" {
 
 module "s3_bucket" {
   source              = "app.terraform.io/shavo/s3-bucket/aws"
-  version             = "3.1.0"
+  version             = "4.7.1"
   s3_object_ownership = "BucketOwnerEnforced"
 }
